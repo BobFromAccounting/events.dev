c60efdd20479f76d523984e9a50059ee2eb41316
Alexandra Gutierrez <agutie95@yahoo.com>
2015-09-16 15:19:06 -0500

    animated homepage

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -9,6 +9,15 @@
     <div class="container">
         <div class="col-md-12 homepage-signup">
             <h1 class="site-name">Gamer-Time</h1>
+            <div id="img-move" class="row">
+            	<img id="r2d2" class="pixel-img" src="/img/r2d2.gif">
+            	<img id="mario" class="pixel-img" src="/img/mariopixel.png">
+        		<img id="pokemon" class="pixel-img" src="/img/pokemon.png">
+            </div>
         </div>
     </div>
+@stop
+
+@section('script')
+	<script type="text/javascript" src="/js/home.js"></script>
 @stop
---
+++ public/js/home.js
@@ -0,0 +1,11 @@
+(function() {
+'use strict';
+	
+	$(document).ready(function(){
+		$(".pixel-img").animate({
+		    right: "1250px",
+		}, 5000);
+		
+	});
+
+})();
