db1726be5e3e284ef2516bf4582a63645adfb45a
Alexandra Gutierrez <agutie95@yahoo.com>
2015-09-17 14:56:02 -0500

    styled and edited forms

--- app/views/events/create.blade.php
+++ app/views/events/create.blade.php
@@ -29,7 +29,7 @@
 				<div class="row">
 					{{ Form::label('where', 'Where') }}<br>
 					<div class="dropdown form-group col-md-3" id="location">
-						{{ Form::select('location_dropdown', $dropdown, null, ['class' => 'form-control dropdown-toggle btn btn-default', 'id' => 'location-dropdown']) }}
+						{{ Form::select('location_dropdown', $dropdown, null, ['class' => 'form-control dropdown-toggle btn btn-default form-dropdown']) }}
 					</div>
 				</div>
 
@@ -47,7 +47,7 @@
 						{{ Form::text('city', null, ['class' => 'form-control', 'placeholder' => 'City']) }}
 					</div>
 					<div class="dropdown form-group col-md-3" id="state">
-						{{ Form::select('state', Config::get('states'), null, ['class' => 'form-control dropdown-toggle btn btn-default' ]) }}
+						{{ Form::select('state', Config::get('states'), null, ['class' => 'form-control dropdown-toggle btn btn-default form-dropdown']) }}
 					</div>
 					<div class="form-group col-md-4" id="zip">
 						{{ Form::number('zip', null, ['class' => 'form-control', 'placeholder' => 'Zip']) }}
@@ -58,10 +58,10 @@
 			<div class="row">
 				{{ Form::label('game', 'Game') }}<br>
 				<div class="dropdown form-group col-md-3" id="console">
-	  				{{ Form::select('console', Config::get('devices'), null, ['class' => 'form-control dropdown-toggle btn btn-default' ]) }}
+	  				{{ Form::select('console', Config::get('devices'), null, ['class' => 'form-control dropdown-toggle btn btn-default form-dropdown']) }}
 				</div>
 				<div class="dropdown form-group col-md-3" id="genre">
-					{{ Form::select('genre', Config::get('genres'), null, ['class' => 'form-control dropdown-toggle btn btn-default' ]) }}
+					{{ Form::select('genre', Config::get('genres'), null, ['class' => 'form-control dropdown-toggle btn btn-default form-dropdown']) }}
 
 				</div>
 				<div class="form-group col-md-6" id="games">
@@ -83,7 +83,7 @@
 				</div>
 			</div>
 
-			{{ Form::submit('Save', ['class' => 'btn btn-large btn-default', 'id' => 'save']) }}
+			{{ Form::submit('Save', ['class' => 'btn btn-default form-control form-submit', 'id' => 'save']) }}
 		</div>
 	</div>
 @stop
