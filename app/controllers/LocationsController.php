53a0168f0ed3f2a3b62313594fd67a85bad6c509
Tarek S Hafez <thafez@ymail.com>
2015-09-17 15:24:59 -0500

    CalendarEventsController.php updated index to allow for search by states. LocationsController.php was edited to remove all funcitons. UsersController.php edited to include app aborts. Routes removed for consolidated search results. master updated for scrollable dropdowns. sass files updated with proper syntax for scrolling ddmenus.:

--- app/controllers/LocationsController.php
+++ app/controllers/LocationsController.php
@@ -8,26 +8,4 @@ class LocationsController extends \BaseController {
 	 *
 	 * @return Response
 	 */
-	public function index()
-	{
-		//
-	}
-
-	/**
-	 * Display the specified resource.
-	 * GET /locations/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		$location = Location::findOrFail($id);
-		
-		if (!$location){
-			App::abort(404);
-		}
-
-		return View::make('events.show')->with('event', $event);
-	}
 }
