e2217126725a1a39e15d9f236d7bfd676d3857f0
Alexandra Gutierrez <agutie95@yahoo.com>
2015-09-18 10:03:38 -0500

    added images to console page - no filters

--- app/views/consoles.blade.php
+++ app/views/consoles.blade.php
@@ -4,10 +4,50 @@
     <div class="container">
     	<div class="col-md-2"></div>
         <div class="col-md-8">
-            <a class="console-img" href="#"><img src="http://placehold.it/350x300"></a>
-            <a class="console-img" href="#"><img src="http://placehold.it/350x300"></a>
-            <a class="console-img" href="#"><img src="http://placehold.it/350x300"></a>
-            <a class="console-img" href="#"><img src="http://placehold.it/350x300"></a>
+            <div class="row console-img-canvas">
+                <a href="#"><img class="console-img" src="/img/consoles/Nintendo-NES_360.jpg"></a>
+                <a href="#"><img class="console-img" src="/img/consoles/super-nintendo.png"></a>
+            </div>
+
+            <div class="row console-img-canvas">
+                <a href="#"><img class="console-img" src="/img/consoles/Nintendo64.jpg"></a>
+                <a href="#"><img class="console-img" src="/img/consoles/nintendo-wii.jpg"></a>
+            </div>
+            
+            <div class="row console-img-canvas">
+                <a href="#"><img class="console-img" src="/img/consoles/nintendo_wii_u.jpg"></a>
+                <a href="#"><img class="console-img" src="/img/consoles/playstation_one.jpg"></a>
+            </div>
+            
+            <div class="row console-img-canvas">
+                <a href="#"><img class="console-img" src="/img/consoles/playstation_two.jpeg"></a>
+                <a href="#"><img class="console-img" src="/img/consoles/playstation_three.jpg"></a>
+            </div>
+
+            <div class="row console-img-canvas">
+                <a href="#"><img class="console-img" src="/img/consoles/playstation_four.jpg"></a>
+                <a href="#"><img class="console-img" src="/img/consoles/Sega-Genesis.jpg"></a>
+            </div>            
+
+            <div class="row console-img-canvas">
+                <a href="#"><img class="console-img" src="/img/consoles/dreamcast.png"></a>
+                <a href="#"><img class="console-img" src="/img/consoles/Xbox.jpg"></a>
+            </div>            
+
+            <div class="row console-img-canvas">
+                <a href="#"><img class="console-img" src="/img/consoles/Xbox-360.jpg"></a>
+                <a href="#"><img class="console-img" src="/img/consoles/Xbox-One.jpg"></a>
+            </div>
+
+            <div class="row console-img-canvas">
+                <a href="#"><img class="console-img" src="/img/consoles/PC.jpg"></a>
+                <a href="#"><img class="console-img" src="/img/consoles/imac.jpg"></a>
+            </div>
+
+            <div class="row console-img-canvas">
+                <a href="#"><img class="console-img" src="/img/consoles/board-games.jpg"></a>
+                {{-- <a href="#"><img class="console-img" src="http://placehold.it/350x300"></a> --}}
+            </div>
 
         </div>
     	<div class="col-md-2"></div>
